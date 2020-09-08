37ec639d719967b976f964f0e241e8baa4c083d0
Olivier <odutartre@onewealthplace.com>
2020-09-08 20:47:16 +0200

    0x00 : Fix definition

--- 0x00-singly_linked_lists/100-first.c
+++ 0x00-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+/**
+ * tortoise - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ */
 void tortoise(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
