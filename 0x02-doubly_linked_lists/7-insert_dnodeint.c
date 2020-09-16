a733e0198d24d3cc04cd95b81cec8d9ba2340934
Olivier <odutartre@onewealthplace.com>
2020-09-16 16:11:16 +0200

    0x02 : Fix insert

--- 0x02-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x02-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,29 +9,27 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current, *new;
-	unsigned int imax;
+	unsigned int len;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 	current = *h;
-	imax = 0;
+	len = 0;
 	while (current != NULL)
 	{
 		current = current->next;
-		imax++;
+		len++;
 	}
-	if (imax < idx)
+	if (idx > len)
 		return (NULL);
-	if (imax == idx)
+	if (idx == len)
 		return (add_dnodeint_end(h, n));
 	current = *h;
-	while (idx > 1)
+	idx = idx - 1;
+	while (idx != 0)
 	{
-		printf(">%d\n", idx);
 		current = current->next;
 		idx--;
 	}
@@ -39,11 +37,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-
 	new->next = current->next;
 	new->prev = current;
 	current->next->prev = new;
 	current->next = new;
-
 	return (new);
 }
